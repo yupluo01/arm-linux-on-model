43b6bc2acae61a80d5263c95b51baabec309e986
Yuping Luo <yuping.luo@arm.com>
2020-07-16 16:28:46 +0800

    add linux-next branch for the sync script

--- sync.sh
+++ sync.sh
@@ -2,8 +2,15 @@ git clone -b master git://git.busybox.net/busybox busybox
 git clone -b 20.04 https://git.linaro.org/landing-teams/working/arm/u-boot
 git clone -b master https://github.com/ARMmbed/mbedtls.git
 git clone -b master https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git arm-tf
-git clone -b master git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux
 git clone -b master https://git.linaro.org/landing-teams/working/arm/ramdisk
+
+git clone -b master git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git linux
+cd linux
+git remote add linux-next https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git
+git fetch linux-next
+git checkout master
+git remote update
+cd ..
 
 #mkdir -p optee
 #cd optee
